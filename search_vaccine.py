80b9f49822a5433f798c0f0d509fa22b9cc13fc2
akash.dubey <akash.dubey@salesforce.com>
2021-05-07 16:04:23 +0530

    dont failed on json decoder and show better error message

--- search_vaccine.py
+++ search_vaccine.py
@@ -2,14 +2,16 @@
 # -*- coding: utf-8 -*-
 import requests
 import time
-from datetime import date
+from datetime import date, datetime
+import json
 
 
 # for jabalpur district id is 315, ask dev to find out the id if you are using
 # it for someother location
-
 district = "315"
-age = 45
+
+# age could be either 18 or 45, depending on need
+age = 18
 
 # Enter the time interval for which you want the slots to be checked (in seconds) Default is 20 mins.
 time_interval = 1200
@@ -21,16 +23,21 @@ url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistri
 #import pdb; pdb.set_trace()
 
 while True:
+    print('--------',datetime.now(),'--------')
     msg_body = ''
-    data = requests.get(url)
+    try:
+        data = requests.get(url)
+    except json.decoder.JSONDecodeError:
+        print('json decoder issue, may be no response from api')
     for entry in data.json()['sessions']:
         if entry['available_capacity'] > 0 \
            and entry['min_age_limit'] == age:
            send_msg = True
            msg_body = "Available {} slots for {}+ in {}  , pin: {} ".format(entry['available_capacity'
                 ], entry['min_age_limit'], entry['name'] , entry['pincode'])
-           
+
            print(msg_body)
         else:
-           print('No slots, checked at : ', datetime.datetime.now())
+           print('No vaccine slot. checked at : ', datetime.now())
     time.sleep(time_interval)
+
